e9f4b4934138e7d3119ba5c4792d3310df6e6964
Renata Cardoso <renata.cardoli@gmail.com>
2021-09-28 12:07:55 -0300

    Creacion de la pagina index.html y main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let numero1 = parseInt(prompt("ingrese un numero:"));
+let numero2 = parseInt(prompt("ingrese un nuevo numero:"));
+let numero3 = parseInt(prompt("ingrese un nuevo numero:"));
+
+let resultado = numero1 + numero2 + numero3;
+
+alert("El resultado de la suma es:" + resultado)
